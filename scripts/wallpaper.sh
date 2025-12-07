4154eabf10bd4c7c4638ed552227d839708a4e86
Yosevian Misyael <132020551+yosmisyael@users.noreply.github.com>
2025-12-07 18:17:43 +0700

    feat: update wallpaper switcher script

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -2,10 +2,29 @@
 
 WALL_DIR="$HOME/.config/wallpaper"
 
-SELECTED_FILE=$(find "$WALL_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' \) \
-                -printf "%f\0icon\037%p\n" | rofi -dmenu -p "" -theme ~/.config/rofi/wallpaper.rasi)
+# fetch all wallpapers
+mapfile -d '' files_full_path < <(find "$WALL_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' \) -print0)
 
-if [ -n "$SELECTED_FILE" ]; then
-    FULL_PATH="$WALL_DIR/$SELECTED_FILE"
-    "$HOME/.config/vesper/scripts/theme.sh" $FULL_PATH
+# prepare them as rofi input
+SELECTED_FILENAME=$(
+    for path in "${files_full_path[@]}"; do
+        filename="${path##*/}"
+        printf "%s\0icon\x1f%s\n" "$filename" "$path"
+    done | rofi -dmenu -p "Select Wallpaper" -show-icons -theme ~/.config/rofi/wallpaper.rasi
+)
+
+# get the selected item full path
+if [ -n "$SELECTED_FILENAME" ]; then
+    SELECTED_PATH=""
+    for path in "${files_full_path[@]}"; do
+        if [[ "${path##*/}" == "$SELECTED_FILENAME" ]]; then
+            SELECTED_PATH="$path"
+            break
+        fi
+    done
+
+    # execute theme script
+    if [ -n "$SELECTED_PATH" ]; then
+        "$HOME/.config/vesper/scripts/theme.sh" "$SELECTED_PATH"
+    fi
 fi
